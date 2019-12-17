551eb250bd9f1bacebfe5e4d427899ea3e092673
Joe Cipolla <jlcipolla@gmail.com>
2019-12-17 15:39:41 -0600

    testing reward signals

---
+++ reward_test.py
@@ -0,0 +1,68 @@
+# Reward Signal testing to insure:
+# 1. Lander will crash at accelerated velocity (y_velocity < -3);
+#    downward velocity greater than 3
+# 2. Lander will crash at accelerated horizontal velocity
+#    (x_velocity < -10 or 10 < x_velocity)
+# 3. Lander will crash at non-zero vetical position, with 5 degree acceptable variance
+#    (5 < angle < 355 ; vertical is at 0 degrees, and lander can be between [0, 359])
+# 4. Lander will crash if it runs out of fuel (fuel <= 0)
+# 5. Least amount of fuel used possible (to save money)
+# 6. Lander will crash if not landed within landing zone (x_position != landing_zone)
+
+import environment
+from utils import get_landing_zone, get_angle, get_velocity, get_position, get_fuel, tests
+get_landing_zone()
+# Lunar Lander Environment
+class LunarLanderEnvironment(environment.BaseEnvironment):
+    def __init__(self):
+        self.current_state = None
+        self.count = 0
+
+    def env_init(self, env_info):
+        # users set this up
+        self.state = np.zeros(6) # velocity x, y, angle, distance to ground, landing zone x, y
+
+    def env_start(self):
+        land_x, land_y = get_landing_zone() # gets the x, y coordinate of the landing zone
+        # At the start we initialize the agent to the top left hand corner (100, 20) with 0 velocity
+        # in either any direction. The agent's angle is set to 0 and the landing zone is retrieved and set.
+        # The lander starts with fuel of 100.
+        # (vel_x, vel_y, angle, pos_x, pos_y, land_x, land_y, fuel)
+        self.current_state = (0, 0, 0, 100, 20, land_x, land_y, 100)
+        return self.current_state
+
+    def env_step(self, action):
+
+        land_x, land_y = get_landing_zone() # gets the x, y coordinate of the landing zone
+        vel_x, vel_y = get_velocity(action) # gets the x, y velocity of the lander
+        angle = get_angle(action) # gets the angle the lander is positioned in
+        pos_x, pos_y = get_position(action) # gets the x, y position of the lander
+        fuel = get_fuel(action) # get the amount of fuel remaining for the lander
+
+        terminal = False
+        reward = 0.0
+        observation = (vel_x, vel_y, angle, pos_x, pos_y, land_x, land_y, fuel)
+
+        # use the above observations to decide what the reward will be, and if the
+        # agent is in a terminal state.
+        # Recall - if the agent crashes or lands terminal needs to be set to True
+
+        # YOUR CODE HERE
+        raise NotImplementedError()
+
+        self.reward_obs_term = (reward, observation, terminal)
+        return self.reward_obs_term
+
+    def env_cleanup(self):
+        return None
+
+    def env_message(self):
+        return None
+
+tests(LunarLanderEnvironment, 1)
+
+tests(LunarLanderEnvironment, 2)
+
+tests(LunarLanderEnvironment, 3)
+
+tests(LunarLanderEnvironment, 4)
